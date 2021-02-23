65e97eb7559ac1ff44705d5251acbcecf737448e
Bit <randomgamerhun@gmail.com>
2021-02-24 00:21:03 +0100

    Added background generation for snake game
    Please don't resize the window

--- Logic-games/Snake.cs
+++ Logic-games/Snake.cs
@@ -12,9 +12,43 @@ namespace Logic_games
 {
     public partial class Snake : Form
     {
+        private int xOffset = 0;
+        private int yOffset = 0;
+        private int tileSize = 40;
+        private Color backColor1 = Color.FromArgb(170, 215, 85);
+        private Color backColor2 = Color.FromArgb(162, 209, 73);
+
         public Snake()
         {
             InitializeComponent();
+        }
+
+        private void Snake_Load(object sender, EventArgs e)
+        {
+            Width = 1016;
+            Height = 1039;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            DrawBackGround();
+        }
+
+        private void DrawBackGround() {
+            Graphics g = CreateGraphics();
+            Brush backPen1 = new SolidBrush(backColor1);
+            Brush backPen2 = new SolidBrush(backColor2);
+            bool activePen = true;
+
+            for (int i = yOffset; i < Height-tileSize; i += tileSize)
+            {
+                for (int j = xOffset; j < Width-tileSize; j += tileSize)
+                {
+                    Rectangle rec = new Rectangle(j, i, tileSize, tileSize);
+                    g.FillRectangle(activePen ? backPen1 : backPen2, rec);
+                    activePen = !activePen;
+                }
+            }
         }
     }
 }
