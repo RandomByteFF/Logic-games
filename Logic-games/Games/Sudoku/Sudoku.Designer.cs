ea007d1327476de6ed255d79d604951b6313197e
Ariadra <ajsa.posta@gmail.com>
2021-02-27 20:50:47 +0100

    Sudoku table

--- Logic-games/Games/Sudoku/Sudoku.Designer.cs
+++ Logic-games/Games/Sudoku/Sudoku.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(364, 364);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // Sudoku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(521, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Sudoku";
             this.Text = "Sudoku";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Logic-games/Games/Sudoku/Sudoku.cs
+++ Logic-games/Games/Sudoku/Sudoku.cs
@@ -7,14 +7,111 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Logic_games
 {
     public partial class Sudoku : Form
     {
+        static int[,] meg = new int[9, 9];
         public Sudoku()
         {
             InitializeComponent();
+            meg = Fel();
+            int[,] val = new int[9, 9];
+            dataGridView1.DefaultCellStyle.SelectionBackColor = dataGridView1.DefaultCellStyle.BackColor;
+            dataGridView1.DefaultCellStyle.SelectionForeColor = dataGridView1.DefaultCellStyle.ForeColor;
+            for (int i = 0; i < 9; i++)
+            {
+                dataGridView1.Columns.Add(string.Empty, string.Empty);
+                dataGridView1.Columns[i].Width = 40;
+                if (i != 9)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[i].Height = 40;
+                }
+
+                Random v = new Random();
+                int vv = v.Next(2, 4);
+                List<int> ki = new List<int>();
+                while (ki.Count < vv)
+                {
+                    Random rand = new Random();
+                    int r = rand.Next(0, 9);
+                    if (!ki.Contains(r))
+                    {
+                        ki.Add(r);
+                    }
+                }
+                for (int j = 0; j < 9; j++)
+                {
+                    if (!ki.Contains(j))
+                    {
+                        val[i, j] = meg[i, j];
+                    }
+                   
+                }
+            }
+            for(int i = 0; i < 9; i++)
+            {
+                for ( int j = 0; j < 9; j++)
+                {
+                    dataGridView1[j, i].ReadOnly = true;
+                    if (val[i, j] != 0)
+                    {
+                        dataGridView1[j, i].Value = val[i, j];
+                    }
+                    else
+                    {
+                        dataGridView1[j, i].ReadOnly = false;
+                    }
+                }
+            }
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            
+        }
+
+        static int[,] Fel()
+        {
+            int[,] meg = new int[9, 9];
+            string con = "server=localhost;user=root;database=Sudoku;password=''";
+            MySqlConnection filo = new MySqlConnection(con);
+            filo.Open();
+            string pille = "megoldas";
+            Random val = new Random();
+            pille += Convert.ToString(val.Next(1, 4));
+            string ms = "SELECT * FROM " + pille + ";";
+            MySqlCommand mc = new MySqlCommand(ms, filo);
+            MySqlDataReader rdr = mc.ExecuteReader();
+            int i = 0;
+            while (rdr.Read())
+            {
+                for(int j = 0; j < 9; j++)
+                 {
+                     meg[i, j] = Convert.ToInt32(rdr[j]);
+                 }
+                i++;
+            }
+            filo.Close();
+            return meg;
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString() == meg[e.RowIndex,e.ColumnIndex].ToString())
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].ReadOnly = true;
+                }
+                else
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.ForeColor = Color.Red;
+                }
+            }
         }
     }
 }
