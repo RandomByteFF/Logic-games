8936c68f92fff6cf8f50266cfb96eca4c99fb5dc
Ariadra <ajsa.posta@gmail.com>
2021-03-09 23:00:52 +0100

    Help system

--- Logic-games/Games/Sudoku/Sudoku.Designer.cs
+++ Logic-games/Games/Sudoku/Sudoku.Designer.cs
@@ -30,6 +30,10 @@
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,20 +56,68 @@
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(186, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(416, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Segítség";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(413, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Segitség száma:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(433, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
             // Sudoku
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(521, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Sudoku";
             this.Text = "Sudoku";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Logic-games/Games/Sudoku/Sudoku.cs
+++ Logic-games/Games/Sudoku/Sudoku.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using System.IO;
 
 namespace Logic_games
 {
@@ -16,10 +17,148 @@ namespace Logic_games
     {
         static int mt = 0;
         static int red = 0;
+        static int[,] meg = new int[9, 9];
         public Sudoku()
         {
             InitializeComponent();
-            int[,] meg = Fel();
+            dataGridView1.Visible = false;
+            button2.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+        }
+
+
+        static int[,] Fel()
+        {
+            /*string con = "server=localhost;user=root;database=logicgames;password=''";
+            MySqlConnection filo = new MySqlConnection(con);
+            filo.Open();
+            string pille = "megoldas";
+            
+            //
+            pille += Convert.ToString(1);
+            string ms = "SELECT * FROM " + pille + ";";
+            MySqlCommand mc = new MySqlCommand(ms, filo);
+            MySqlDataReader rdr = mc.ExecuteReader();
+            int i = 0;
+            while (rdr.Read())
+            {
+                for(int j = 0; j < 9; j++)
+                 {
+                     meg[i, j] = Convert.ToInt32(rdr[j]);
+                 }
+                i++;
+            }
+            filo.Close();*/
+            int[,] meg = new int[9, 9];
+            string pille = "megoldas";
+            Random val = new Random();
+            pille += Convert.ToString(val.Next(1, 4));
+            string[] m = File.ReadAllLines(pille + ".txt");
+            for (int i = 0; i < m.Length; i++)
+            {
+                string[] line = m[i].Split(' ');
+                for (int j = 0; j < 9; j++)
+                {
+                    meg[i, j] = Convert.ToInt32(line[j]);
+                }
+            }
+            return meg;
+        }
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                string val = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = null;
+                bool gode = true;
+                int i = 0;
+                while (gode && i < 9)
+                {
+                    if (dataGridView1.Rows[e.RowIndex].Cells[i].Value != null)
+                    {
+                        if (dataGridView1.Rows[e.RowIndex].Cells[i].Value.ToString() == val) { gode = false; }
+                    }
+                    if (dataGridView1.Rows[i].Cells[e.ColumnIndex].Value != null)
+                    {
+                        if (dataGridView1.Rows[i].Cells[e.ColumnIndex].Value.ToString() == val) { gode = false; }
+                    }
+                    i++;
+                }
+                int j = 0;
+                int k = 0;
+                if (e.ColumnIndex < 3) { j = 2; }
+                else if (e.ColumnIndex < 6) { j = 5; }
+                else { j = 8; }
+                if (e.RowIndex < 3) { i = 0; k = 3; }
+                else if (e.RowIndex < 6) { i = 3; k = 6; }
+                else { i = 6; k = 9; }
+                while (gode && i < k)
+                {
+                    if (dataGridView1.Rows[i].Cells[j - 2].Value != null)
+                    {
+                        if (dataGridView1.Rows[i].Cells[j - 2].Value.ToString() == val) { gode = false; }
+                    }
+                    if (dataGridView1.Rows[i].Cells[j - 1].Value != null)
+                    {
+                        if (dataGridView1.Rows[i].Cells[j - 1].Value.ToString() == val) { gode = false; }
+                    }
+                    if (dataGridView1.Rows[i].Cells[j].Value != null)
+                    {
+                        if (dataGridView1.Rows[i].Cells[j].Value.ToString() == val) { gode = false; }
+                    }
+                    i++;
+                }
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = val;
+                if (gode)
+                {
+                    if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.ForeColor == Color.Red) red--;
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.ForeColor = Color.DarkBlue;
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].ReadOnly = true;
+                    mt--;
+                }
+                else
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.ForeColor = Color.Red;
+                    red++;
+                }
+                if (mt == 0 && red == 0)
+                {
+                    MessageBox.Show("Gratulálok nyertél!");
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (mt != 0 && label2.Text != "0")
+            {
+                Random v = new Random();
+                int x = v.Next(0, 9);
+                int y = v.Next(0, 9);
+                while (dataGridView1.Rows[x].Cells[y].Value != null)
+                {
+                    x = v.Next(0, 9);
+                    y = v.Next(0, 9);
+                }
+                dataGridView1.Rows[x].Cells[y].Value = meg[x, y];
+                dataGridView1.Rows[x].Cells[y].Style.Font = new Font("Times New Roman", 14F, FontStyle.Bold);
+                dataGridView1.Rows[x].Cells[y].ReadOnly = true;
+                label2.Text = Convert.ToString(Convert.ToInt32(label2.Text) - 1);
+                mt--;
+                if (mt == 0 && red == 0)
+                {
+                    MessageBox.Show("Gratulálok nyertél!");
+                }
+            }
+            else if (mt != 0)
+            {
+                MessageBox.Show("Sajnos elfogyott a segítség kéréseidnek a száma!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            meg = Fel();
             int[,] val = new int[9, 9];
             dataGridView1.DefaultCellStyle.SelectionBackColor = dataGridView1.DefaultCellStyle.BackColor;
             dataGridView1.DefaultCellStyle.SelectionForeColor = dataGridView1.DefaultCellStyle.ForeColor;
@@ -52,12 +191,12 @@ namespace Logic_games
                     {
                         val[i, j] = meg[i, j];
                     }
-                   
+
                 }
             }
-            for(int i = 0; i < 9; i++)
+            for (int i = 0; i < 9; i++)
             {
-                for ( int j = 0; j < 9; j++)
+                for (int j = 0; j < 9; j++)
                 {
                     dataGridView1[j, i].ReadOnly = true;
                     if (val[i, j] != 0)
@@ -75,95 +214,16 @@ namespace Logic_games
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.AllowUserToResizeColumns = false;
             dataGridView1.AllowUserToResizeRows = false;
-            
-        }
-
-        static int[,] Fel()
-        {
-            int[,] meg = new int[9, 9];
-            string con = "server=localhost;user=root;database=Sudoku;password=''";
-            MySqlConnection filo = new MySqlConnection(con);
-            filo.Open();
-            string pille = "megoldas";
-            Random val = new Random();
-            pille += Convert.ToString(val.Next(1, 4));
-            string ms = "SELECT * FROM " + pille + ";";
-            MySqlCommand mc = new MySqlCommand(ms, filo);
-            MySqlDataReader rdr = mc.ExecuteReader();
-            int i = 0;
-            while (rdr.Read())
-            {
-                for(int j = 0; j < 9; j++)
-                 {
-                     meg[i, j] = Convert.ToInt32(rdr[j]);
-                 }
-                i++;
-            }
-            filo.Close();
-            return meg;
-        }
-        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
-        {
-            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
-            {
-                string val = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
-                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = null;
-                bool gode = true;
-                int i = 0;
-                while(gode && i < 9)
-                {
-                    if(dataGridView1.Rows[e.RowIndex].Cells[i].Value != null)
-                    {
-                        if (dataGridView1.Rows[e.RowIndex].Cells[i].Value.ToString() == val) { gode = false; }
-                    }
-                    if (dataGridView1.Rows[i].Cells[e.ColumnIndex].Value != null)
-                    {
-                        if (dataGridView1.Rows[i].Cells[e.ColumnIndex].Value.ToString() == val) { gode = false; }
-                    }
-                    i++;
-                }
-                int j = 0;
-                int k = 0;
-                if (e.ColumnIndex < 3) { j = 2; }
-                else if (e.ColumnIndex < 6) { j = 5; }
-                else { j = 8; }
-                if (e.RowIndex < 3) { i = 0;k = 3; }
-                else if (e.RowIndex < 6) { i = 3;k = 6; }
-                else { i = 6;k = 9; }
-                while (gode && i < k)
-                {
-                    if (dataGridView1.Rows[i].Cells[j - 2].Value != null)
-                    {
-                        if (dataGridView1.Rows[i].Cells[j - 2].Value.ToString() == val) { gode = false; }
-                    }
-                    if (dataGridView1.Rows[i].Cells[j - 1].Value != null)
-                    {
-                        if (dataGridView1.Rows[i].Cells[j - 1].Value.ToString() == val) { gode = false; }
-                    }
-                    if (dataGridView1.Rows[i].Cells[j].Value != null)
-                    {
-                        if (dataGridView1.Rows[i].Cells[j].Value.ToString() == val) { gode = false; }
-                    }
-                    i++;
-                }
-                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = val;
-                if (gode)
-                {
-                    if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.ForeColor == Color.Red) red--;
-                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.ForeColor = Color.Black;
-                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].ReadOnly = true;
-                    mt--;
-                }
-                else
-                {
-                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.ForeColor = Color.Red;
-                    red++;
-                }
-                if(mt == 0 && red == 0)
-                {
-                    MessageBox.Show("Gratulálok nyertél!");
-                }
-            }
+            dataGridView1.Columns[2].DividerWidth = 5;
+            dataGridView1.Rows[2].DividerHeight = 5;
+            dataGridView1.Columns[5].DividerWidth = 5;
+            dataGridView1.Rows[5].DividerHeight = 5;
+            dataGridView1.Visible = true;
+            button2.Visible = true;
+            button1.Visible = false;
+            label1.Visible = true;
+            label2.Text = (mt / 2).ToString();
+            label2.Visible = true;
         }
     }
 }
